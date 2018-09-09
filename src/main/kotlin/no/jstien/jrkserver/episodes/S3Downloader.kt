36c864d7fcd1e567f13f3f7f8f8814791e8a4cd6
Joakim Stien <joakim.stien@gmail.com>
2018-09-09 11:22:40 +0200

    Added S3-access classes

---
+++ src/main/kotlin/no/jstien/jrkserver/episodes/S3Downloader.kt
@@ -0,0 +1,42 @@
+package no.jstien.jrkserver.episodes
+
+import no.jstien.jrkserver.episodes.segmentation.FFMPEGSegmenter
+import no.jstien.jrkserver.util.ProcessExecutor
+import org.apache.logging.log4j.LogManager
+import java.util.concurrent.CompletableFuture
+
+private const val TARGET_SEGMENT_DURATION = 10
+
+class S3Downloader(fileRepository: S3FileRepository) {
+    companion object {
+        private val LOGGER = LogManager.getLogger()
+    }
+
+    private val episodeRepository = fileRepository
+    private var episodeTask = startNextDownload()
+
+    fun getNextEpisode(): Episode {
+        val episode = episodeTask.get()
+        episodeTask = startNextDownload()
+        return episode
+    }
+
+    private fun startNextDownload(): CompletableFuture<Episode> {
+        return CompletableFuture.supplyAsync { downloadEpisode() }
+    }
+
+    private fun downloadEpisode(): Episode {
+        LOGGER.info("Starting preparation of episode")
+
+        val s3Key = episodeRepository.popRandomS3Key()
+        val path = episodeRepository.downloadFile(s3Key)
+
+        val segmentationRequest = SegmentationRequest(TARGET_SEGMENT_DURATION, path, s3Key)
+        val segmenter = FFMPEGSegmenter(ProcessExecutor())
+        val episode = segmenter.segmentFile(segmentationRequest)
+
+        LOGGER.info("Episode prepared")
+        return episode
+    }
+
+}
